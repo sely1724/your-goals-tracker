0793c86936ab4b39f14d8cad4220861392bcf340
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2023-01-10 20:19:04 -0800

    connection.js file is done

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+// first connect to HEROKU if deployed, else localhost   
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
