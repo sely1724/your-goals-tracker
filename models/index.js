a580e7a345d3accd8fa2c12a5961e4f403325955
Jacob Meyers <jacobmeyers6@outlook.com>
2023-01-12 19:19:57 -0800

    added FK references in index

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
 const User = require('./User');
 const Goal = require('./Goal');
 
-User.hasMany(Goal);
-Goal.belongsTo(User);
+User.hasMany(Goal, {
+   foreignKey: 'user_id'
+});
+Goal.belongsTo(User, {
+   foreignKey: 'user_id'
+});
 
 module.exports = { Goal, User };
