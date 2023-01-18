24e9da6ce29506168033866962a23005f4d2fc8b
Jacob Meyers <jacobmeyers6@outlook.com>
2023-01-17 18:51:37 -0800

    added dom hooks

---
+++ public/js/login.js
@@ -0,0 +1,9 @@
+const loginFormEl = Document.querySelector(".login-form");
+const loginEmailInputEl = Document.querySelector("#email-login");
+const loginPasswordInputEl = Document.querySelector("#password-login");
+
+const signupFormEl = Document.querySelector(".signup-form")
+const signupUsernameInputEl = Document.querySelector("#username-signup");
+const signupEmailInputEl = Document.querySelector("#email-signup");
+const signupPasswordInputEl = Document.querySelector("#password-signup");
+
