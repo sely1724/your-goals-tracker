ef3b458f9630a00359e1af7e548dcfb3420ded13
Jacob Meyers <jacobmeyers6@outlook.com>
2023-01-17 19:19:39 -0800

    added signup route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,62 +1,76 @@
 const router = require('express').Router();
+const { STRING } = require('sequelize');
 const { User } = require('../../models');
 
 // Login
 router.post('/login', async (req, res) => {
-    try {
-      const dbUserData = await User.findOne({
-        where: {
-            email: req.body.email,
-        },
-      });
-  
-      if (!dbUserData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email. Please try again!' });
-        return;
-      }
-  
-      const validPassword = await dbUserData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect password. Please try again!' });
-        return;
-      }
-  
-      // Once the user successfully logs in, set up the sessions variable 'loggedIn'
-      req.session.save(() => {
-        req.session.loggedIn = true;   // loggedIn varaible is set to browser to mark session as loged in
-        req.session.userId = dbUserData.id;
-        res
-          .status(200)
-          .json({ user: dbUserData, message: 'You are now logged in!' });
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email. Please try again!' });
+      return;
     }
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect password. Please try again!' });
+      return;
+    }
+
+    // Once the user successfully logs in, set up the sessions variable 'loggedIn'
+    req.session.save(() => {
+      req.session.loggedIn = true;   // loggedIn varaible is set to browser to mark session as loged in
+      req.session.userId = dbUserData.id;
+      res
+        .status(200)
+        .json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // Logout
 router.post('/logout', (req, res) => {
-    // When the user logs out, destroy the session
-    if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
+  // When the user logs out, destroy the session
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 // Sign up
-router.post('/signup', (req, res) => {
+// body format: {
+//   username: STRING,
+//   email: STRING,
+//   password: STRING
+// }
+router.post('/signup', async (req, res) => {
   try {
-    
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.email
+    });
+    res.status(200).json({ comment: dbUserData, message: 'New account created!' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
 })
-  
-  module.exports = router;
+
+module.exports = router;
