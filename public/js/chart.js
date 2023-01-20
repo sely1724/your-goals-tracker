71471eba8d026b69e51956cd2176acd050602e4e
Jacob Meyers <jacobmeyers6@outlook.com>
2023-01-19 19:17:41 -0800

    WIP chart.js

--- public/js/chart.js
+++ public/js/chart.js
@@ -3,5 +3,6 @@ const Chart = require('chartjs');
 Chart.defaults.color = '#153243';
 
 async function render(userID) {
-   
+   const fetchURL = 'api/goal/userid/' + userID
+   const goalData = await fetch(fetchURL, )
 }
