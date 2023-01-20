506c9ea7eb5df90b6592453bb7d97a25bc373b5d
Jacob Meyers <jacobmeyers6@outlook.com>
2023-01-19 20:20:17 -0800

    WIP chart.js, fetch not defined err?

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,11 +1,47 @@
 const Chart = require('chartjs');
-
-Chart.defaults.color = '#153243';
+const moment = require('moment');
+moment().format();
 
 async function render(userID) {
-   const fetchURL = 'api/goal/userid/' + userID
-   const goalData = await fetch(fetchURL, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' }
-   })
+
+   const goalData = await fetch(`api/goal/userid/${userID}`)
+
+   const chartData = parse(goalData);
+
+   console.log(chartData);
+
+   // new Chart(
+   //    document.getElementById('chart'),
+   //    {
+   //       type: bar,
+
+   //    }
+   // )
 }
+
+function parse(goalData) {
+
+   chartData = []
+   // populate array with objects
+   for (let i = 1; i <= 12; i++) {
+      chartData.push({
+         month: moment(i, "M").format("MMM"),
+         goalsCompleted: 0
+      })
+   };
+
+   // iterate through goal data
+   goalData.forEach((goal) => {
+      // if the goal is completed,
+      if (goal.completed) {
+         // add 1 to the goalsCompleted property of the month object
+         // corresponding to the month the goal was completed on
+         chartData.find((month) => month === moment(goal.updatedAt).month())
+            .goalsCompleted += 1;
+      }
+   })
+
+   return chartData;
+}
+
+render(1);
